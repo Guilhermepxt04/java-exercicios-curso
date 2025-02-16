6c0ee71aa934e97956a014f423892deaf66f9e9c
Guilherme Peixoto <guilherme.pxt04@gmail.com>
2025-02-16 20:42:26 -0300

    add exercicios

--- src/EX026.java
+++ src/EX026.java
@@ -8,6 +8,39 @@ public class EX026 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Quantos alunos serão digitados?: ");
+        int n = sc.nextInt();
+
+
+        String[] arrayName = new String[n];
+        Double[] arrayNote1 = new Double[n];
+        Double[] arrayNote2 = new Double[n];
+
+        for (int i=0; i<arrayName.length; i++) {
+
+            System.out.printf("Digite nome, primeira e segunda nota do %d0 aluno: %n", i + 1);
+            sc.nextLine();
+            arrayName[i] = sc.nextLine();
+            arrayNote1[i] = sc.nextDouble();
+            arrayNote2[i] = sc.nextDouble();
+
+
+        }
+
+        System.out.println("Alunos aprovados: ");
+
+        for (int i=0; i< arrayName.length; i++) {
+
+            if ((arrayNote1[i] + arrayNote2[1]) / 2 >= 6.0) {
+
+                System.out.println(arrayName[i]);
+
+            }
+
+        }
+
+
+        sc.close();
     }
 
 }
