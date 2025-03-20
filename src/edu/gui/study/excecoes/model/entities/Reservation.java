bee9dd8bf078600b6f8cb73d90d09e9395d813db
Guilherme Peixoto <guilherme.pxt04@gmail.com>
2025-03-20 13:31:35 -0300

    exercicio Program excecoes

--- src/edu/gui/study/excecoes/model/entities/Reservation.java
+++ src/edu/gui/study/excecoes/model/entities/Reservation.java
@@ -1,11 +1,10 @@
-package edu.gui.study.excecoes.entities;
+package edu.gui.study.excecoes.model.entities;
 
-import java.text.SimpleDateFormat;
+import edu.gui.study.excecoes.model.exception.DomainException;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 public class Reservation {
 
@@ -13,9 +12,11 @@ public class Reservation {
     private LocalDate checkIn;
     private LocalDate checkOut;
 
-    private static DateTimeFormatter sdf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private static final DateTimeFormatter sdf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
+        if (!checkOut.isAfter(checkIn))
+            throw new DomainException("Check-out date must be after check-in date");
         this.roomNumber = roomNumber;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
@@ -27,6 +28,11 @@ public class Reservation {
     }
 
     public void updateDates(LocalDate checkIn, LocalDate checkOut) {
+        LocalDate now = LocalDate.now();
+        if (checkIn.isBefore(now) || checkOut.isBefore(now))
+            throw new DomainException("Reservation dates for update must be future dates");
+        if (!checkOut.isAfter(checkIn))
+            throw new DomainException("Check-out date must be after check-in date");
         this.checkIn = checkIn;
         this.checkOut = checkOut;
     }
---
+++ src/edu/gui/study/excecoes/model/exception/DomainException.java
@@ -0,0 +1,9 @@
+package edu.gui.study.excecoes.model.exception;
+
+
+public class DomainException extends RuntimeException {
+
+    public DomainException(String msg) {
+        super(msg);
+    };
+}
